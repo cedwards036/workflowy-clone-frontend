37ec96038891c4323e6d075849fcdc6245fd9883
Christopher Edwards <cedwards036@gmail.com>
2020-07-04 10:12:15 -0400

    add html rendering functions for node tree

---
+++ src/html.js
@@ -0,0 +1,47 @@
+
+
+const RIGHT_ARROW = '&#9654;';
+const DOWN_ARROW = '&#9660;';
+const DOT = '&#9679;';
+
+export default {
+    forNodeTree(rootNode) {
+        return `
+            <div style="margin-left:${rootNode.level * 30}px;" data-id="${rootNode.id}" class="${this.completedClass(rootNode)}">
+                ${this.forNodeRow(rootNode)}
+                ${this.forChildNodes(rootNode)}
+            </div>
+        `;
+    },
+
+    forNodeRow(node) {
+        return `
+            <div class="node-row">
+                ${this.forNodeArrow(node.isExpanded)}
+                <div class="node-bullet">${DOT}</div>
+                <div class="node-text" contenteditable>${node.text}</div>
+            </div>
+        `;
+    },
+
+    forChildNodes(node) {
+        if (node.isExpanded) {
+            return `
+                <div class="node-children">
+                    ${node.children.map(child => this.forNodeTree(child)).join('')}
+                </div>
+            `;
+        } else {
+            return '';
+        }
+    },
+
+    forNodeArrow(isExpanded) {
+        const arrow = isExpanded ? DOWN_ARROW : RIGHT_ARROW;
+        return `<div class="node-arrow">${arrow}</div>`;
+    },
+
+    completedClass(node) {
+        return node.isCompleted ? 'completed' : '';
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Node from './node';
+import NodeCollection from './node-collection';
+import HTML from './html';
+
+const nodeCollection = NodeCollection()
+                        .add(Node({text: 'root_node', id: 'f983jf', isExpanded: true}))
+                        .add(Node({text: 'go to the store', id: 'v0983f', parentID: 'f983jf', isExpanded: true}))
+                        .add(Node({text: 'wash dishes', id: 'po3933', parentID: 'f983jf'}))
+                        .add(Node({text: 'bread', id: 'vs23f4', parentID: 'v0983f'}))
+                        .add(Node({text: 'milk', id: 'qqt444', parentID: 'v0983f', isCompleted: true}));
+const nodeTree = nodeCollection.buildTree('f983jf');        
+document.getElementById('list').innerHTML = HTML.forNodeTree(nodeCollection.buildTree('f983jf'));           
