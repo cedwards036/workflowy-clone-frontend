0b1a8d46920026c94e9980eefa47601809f378df
Christopher Edwards <cedwards036@gmail.com>
2020-07-06 15:27:59 -0400

    refactor index.js slightly

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import {addEventListeners, loadNodeURL} from './user-interface';
 import {getList, parseGetListResponse} from './api-interface';
 
+const LIST_ID = '5eff5d2dfde52d0004ebd138';
+
 window.onload = () => {
-    getList('5eff5d2dfde52d0004ebd138', e => {
+    getList(LIST_ID, e => {
         const list = parseGetListResponse(e.target.response);
         addEventListeners(list);
-        document.getElementById('showCompletedSwitch').checked = list.showCompleted;
-        if (location.hash === '') {
-            loadNodeURL(`#/${list.currentRootID}`, list);
-        } else {
-            loadNodeURL(location.hash, list);
-        }
+        initializeShowCompletedSwitch(list);
+        initializePage(list);
     });
+}
+
+function initializeShowCompletedSwitch(list) {
+    document.getElementById('showCompletedSwitch').checked = list.showCompleted;
+}
+
+function initializePage(list) {
+    if (location.hash === '') {
+        loadNodeURL(`#/${list.currentRootID}`, list);
+    } else {
+        loadNodeURL(location.hash, list);
+    }
 }
